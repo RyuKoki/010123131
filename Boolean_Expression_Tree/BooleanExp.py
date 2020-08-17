2ae2e041b0e3920b1c7beac77a38ede116b4f355
RyuKoki <68011889+RyuKoki@users.noreply.github.com>
2020-08-18 02:43:22 +0700

    Update BooleanExp.py

--- Boolean_Expression_Tree/BooleanExp.py
+++ Boolean_Expression_Tree/BooleanExp.py
@@ -1,8 +1,8 @@
-# SplitBoolenaExp for spliting expression
-class ExpTree:
+class ExpManage: # ExpMange stands for 'Expression Management'
 
     def __init__(self):
-        self.tree = ['']*50
+        self.result = []
+        self.load = ''
 
     def isOperator(self, e): # e is 'element'
         # method for checking 'Is "e" is operator?'
@@ -10,6 +10,7 @@ class ExpTree:
         return e in operators
 
     def isOperand(self, e): 
+        # method for checking 'Is "e" is in operand?'
         operands = 'I0I1I2I3I4I5I6I7I8I9'
         return e in operands
 
@@ -29,31 +30,38 @@ class ExpTree:
         exp_input = str(exp)
         exp_input = exp_input.replace(" ", "") # for covering when user type 'space bar'
 
-        result = []
-        load = ''
-
         for i in exp_input: # access each element from 'exp'
 
             if self.isOperator(i) or self.isParenth(i):
                 # if element 'i' is operator (isOperator == True),
                 # or element 'i' is parentheses (isParenth == True), 
                 # output is result list() that appends it
-                result.append(i)
+                self.result.append(i)
 
             elif self.isOperand(i):
                 # if element 'i' is operand (isOperand == True), 
                 # load string keeps it until finding operator
-                load += i
+                self.load += i
                 # checking 'Is next of operand 'i' is operator or parantheses
                 check_next_i = exp_input[exp_input.index(i)+1]
                 if self.isOperator(check_next_i) or self.isParenth(check_next_i):
                     # if next of operand 'i' is operator or parantheses
                     # result list() keeps load
-                    result.append(load)
+                    self.result.append(self.load)
                     # then make load empty
-                    load = ''
+                    self.load = ''
 
-        return result
+        return self.result
+
+
+class ExpTree(ExpManage): # ExpTree stands for 'Expression Tree'
+
+    # ExpTree is inheritance from ExpManage
+    def __init__(self):
+        super().__init__()
+        self.tree = []*50
+        self.left_child = []
+        self.right_child = []
 
     def delOuterBrac(self, exp_list):
         # load list() for counting open and close bracket
@@ -75,31 +83,41 @@ class ExpTree:
                     return(exp_list[1:-1])
         return exp_list
 
+    def Tree(self, exp, root, index_r): 
+        # index_r stands for 'index of root'
+        # exp stands for 'expression' into list()
+        open_brac = []
+        exp = self.delOuterBrac(exp)
 
+        CHECK = True
+        for i in exp:
 
+            if CHECK:
 
+                if self.isOpenPar(i):
+                    open_brac.append(i)
+                    index_start = exp.index(i)
 
+                elif self.isClosePar(i):
+                    if len(open_brac) > 1:
+                        open_brac.pop()
+                    elif len(open_brac) == 1:
+                        index_end = exp.index(i)
+                        open_brac.pop()
+                        CHECK = False
 
+            if not CHECK:
 
+                if self.isOperator(i):
+                    if (i == '&') or (i == '+'):
+                        root = i
+                        left = exp[index_start:index_end+1]
+                        right = exp[exp.index(i)+1]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                        
+                        
+                        
+                        
 exp_tree = ExpTree()
 expression = "!(I0&I1)+!(I1+I2)"
 split = exp_tree.Spoolex(expression)
