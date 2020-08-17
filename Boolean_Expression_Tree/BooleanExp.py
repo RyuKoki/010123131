95f17bdcf9c188e74399533f9d1a371480ab0cd9
RyuKoki <68011889+RyuKoki@users.noreply.github.com>
2020-08-17 13:48:43 +0700

    Update BooleanExp.py

--- Boolean_Expression_Tree/BooleanExp.py
+++ Boolean_Expression_Tree/BooleanExp.py
@@ -1,10 +1,8 @@
 # SplitBoolenaExp for spliting expression
-class SplitBooleanExp:
+class ExpTree:
 
-    def __init__(self): # exp is 'expression'
-        self.exp = None
-        self.load = ''
-        self.result = []
+    def __init__(self):
+        self.tree = ['']*50
 
     def isOperator(self, e): # e is 'element'
         # method for checking 'Is "e" is operator?'
@@ -27,10 +25,12 @@ class SplitBooleanExp:
 
     def Spoolex(self, exp): # Spoolex stands for 'Split Boolean Expression'
         # this method for spliting all expression into list()
-        self.exp = str(exp) # always make input 'exp' into string
-
-        exp_input = self.exp
+        # always make input 'exp' into string
+        exp_input = str(exp)
         exp_input = exp_input.replace(" ", "") # for covering when user type 'space bar'
+
+        result = []
+        load = ''
 
         for i in exp_input: # access each element from 'exp'
 
@@ -38,89 +38,74 @@ class SplitBooleanExp:
                 # if element 'i' is operator (isOperator == True),
                 # or element 'i' is parentheses (isParenth == True), 
                 # output is result list() that appends it
-                self.result.append(i)
+                result.append(i)
 
             elif self.isOperand(i):
                 # if element 'i' is operand (isOperand == True), 
                 # load string keeps it until finding operator
-                self.load += i
+                load += i
                 # checking 'Is next of operand 'i' is operator or parantheses
                 check_next_i = exp_input[exp_input.index(i)+1]
                 if self.isOperator(check_next_i) or self.isParenth(check_next_i):
                     # if next of operand 'i' is operator or parantheses
                     # result list() keeps load
-                    self.result.append(self.load)
+                    result.append(load)
                     # then make load empty
-                    self.load = ''
+                    load = ''
 
-        return self.result
-
-# expression = '((I0+!I1+!(I2))&(!I0+I1+I2))'
-# split_exp = SplitBooleanExp()
-# print(split_exp.Spoolex(expression))
-
-class ExpTree(SplitBooleanExp):
-
-    def __init__(self):
-        super().__init__()
-        self.tree = ['']*50
+        return result
 
     def delOuterBrac(self, exp_list):
+        # load list() for counting open and close bracket
         load = []
+        # access all elements from exp_list
         for i in range(len(exp_list)):
             if  self.isOpenPar(exp_list[i]):
+                # if find open bracket >> append to load list()
                 load.append(exp_list[i])
             elif self.isClosePar(exp_list[i]):
+                # if find close bracket
                 if len(load) > 1:
+                    # if open bracket in load list() more than 1
+                    # delete last open bracket in load list()
                     load.pop()
                 elif len(load) == 1 and i == len(exp_list)-1:
+                    # if load list() has only one open bracket and index of the last close bracket
+                    # return value of exp_list from index 1 to index -1
                     return(exp_list[1:-1])
         return exp_list
-   
-    def Tree(self, exp_list, index_root):
-        left = None
-        right = None
-        root = None
-        open_brac = []
-        close_brac = []
 
-        exp_list = self.delOuterBrac(exp_list)
-        
-        for i in range(len(exp_list)):
 
-            if self.isOpenPar(exp_list[i]):
-                open_brac.append(exp_list[i])
 
-            elif self.isClosePar(exp_list[i]):
-                close_brac.append(exp_list[i])
 
-            elif self.isOperator(exp_list[i]):
-                if exp_list[i] == '&' or exp_list[i] == '+':
-                    if len(open_brac) == len(close_brac):
-                        root = exp_list[i]
-                        left = exp_list[:i]
-                        right = exp_list[i+1:]
-                        open_brac.clear()
-                        close_brac.clear()
 
-                elif exp_list[i] == '!':
-                    root = exp_list[i]
-                    left = exp_list[i+1:]
-                    right = None
 
-        index_left = (2*index_root)+1
-        index_right = (2*index_root)+2
 
-        self.tree[index_root] = root
-        self.tree[index_left] = left
-        self.tree[index_right] = right
 
-        print(f'test:{exp_list} \nleft: {left} \nroot: {root} \nright: {right}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 exp_tree = ExpTree()
-exp_input = '((I0+!I1+!(I2))&(!I0+I1+I2))'
-exp_tree.Tree(exp_input, 0)
+expression = "!(I0&I1)+!(I1+I2)"
+split = exp_tree.Spoolex(expression)
+print(split)
+outer_brac = exp_tree.delOuterBrac(split)
+print(outer_brac)
 
-# There are some error! (ToT)
-# I will do it more. But now, i'm losing all enthusiasm.
-# New code will be soon.
+# more code will be coming soon
+# Thank you
