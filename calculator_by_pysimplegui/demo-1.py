f950958422f8e804ac835432e8898c5a5c1f62e8
RyuKoki <68011889+RyuKoki@users.noreply.github.com>
2020-08-30 13:55:08 +0700

    Update demo-1.py

--- calculator_by_pysimplegui/demo-1.py
+++ calculator_by_pysimplegui/demo-1.py
@@ -1,30 +1,43 @@
+############################################################################
+# Author : Onpinya Phokhahutthakosol
+# File Name : demo-1.py
+############################################################################
 import PySimpleGUI as sg 
 
 sg.theme('DarkGreen5')
 
-layout =  [ [sg.Text('0', size=(17, 1), justification='right', background_color='white',
-                        text_color='black', font=('Consolas', 50), key='DISPLAY')],
-            [sg.Button('{}'.format(str(i)), size=(5, 1), font=('Consolas', 30))for i in range(7, 10)] + 
-                [sg.Button('DEL', size=(5, 1), font=('Consolas', 30)), 
-                    sg.Button('AC', size=(5, 1), font=('Consolas', 30))],
-            [sg.Button('{}'.format(str(i)), size=(5, 1), font=('Consolas', 30))for i in range(4, 7)] + 
-                [sg.Button('*', size=(5, 1), font=('Consolas', 30)), 
-                    sg.Button('/', size=(5, 1), font=('Consolas', 30))],
-            [sg.Button('{}'.format(str(i)), size=(5, 1), font=('Consolas', 30))for i in range(1, 4)] + 
-                [sg.Button('+', size=(5, 1), font=('Consolas', 30)), 
-                    sg.Button('-', size=(5, 1), font=('Consolas', 30))],
-            [sg.Button('0', size=(10, 1), font=('Consolas', 30)),
-                sg.Button('.', size=(5, 1), font=('Consolas', 30)),
-                sg.Button('=', size=(10, 1), font=('Consolas', 30))]    ] 
+digit_button = {'size':(4, 1), 'font':('Consolas', 30)}
+operator_button = {'size':(4, 1), 'font':('Consolas', 30)}
+special_button = {'size':(9, 1), 'font':('Consolas', 29)}
 
-window = sg.Window('Calculator', layout)
+layout =  [ [sg.Text('0', size=(14, 1), justification='right', background_color='white',
+                        text_color='black', font=('Consolas', 49), key='DISPLAY')],
+            [sg.Button('{}'.format(str(i)), **digit_button)for i in range(7, 10)] + 
+                [sg.Button('DEL', **operator_button, button_color=('white', 'black')), 
+                    sg.Button('AC', **operator_button, button_color=('white', '#800000'))],
+            [sg.Button('{}'.format(str(i)), **digit_button)for i in range(4, 7)] + 
+                [sg.Button('*', **operator_button, button_color=('white', '#00ba82')), 
+                    sg.Button('/', **operator_button, button_color=('white', '#00ba82'))],
+            [sg.Button('{}'.format(str(i)), **digit_button)for i in range(1, 4)] + 
+                [sg.Button('+', **operator_button, button_color=('white', '#00ba82')), 
+                    sg.Button('-', **operator_button, button_color=('white', '#00ba82'))],
+            [sg.Button('0', **special_button),
+                sg.Button('.', **operator_button),
+                sg.Button('=', **special_button, button_color=('white', 'orange'))]    ] 
+
+window = sg.Window('Simple Calculator', layout=layout, background_color="#272533", return_keyboard_events=True)
 
 running = True
-
+var = ''
 while running:
 
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
 
+    if event in '0123456789':
+        var += event
+        window['DISPLAY'].update(var)
+
 window.close()
+# comming soon
