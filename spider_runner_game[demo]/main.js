cd30bce2395423a236241ef52cb1d0ee96db7d6d
RyuKoki <68011889+RyuKoki@users.noreply.github.com>
2020-09-29 17:20:54 +0700

    Update main.js

--- spider_runner_game[demo]/main.js
+++ spider_runner_game[demo]/main.js
@@ -4,39 +4,34 @@ let context = canvas.getContext('2d');
 canvas.width = window.innerWidth-100;
 canvas.height = 300;
 
-class Player {
-
-    constructor (x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-
-    Draw () {
-        context.fillStyle = 'green';
-        context.fillRect(this.x, this.y, this.width, this.height);
-    }
-}
-let spider_size = 75;
-const player = new Player(50, canvas.height-spider_size, spider_size, spider_size);
-player.Draw();
-
-class Enemy {
-
-    constructor (x) {
-        this.x = x;
-        this.width = Math.floor(Math.random() * (75-50)) + 50;
-        this.height = this.width;
-        this.y = canvas.height - this.height;
-    }
-
-    Draw () {
-        context.fillStyle = 'red';
-        context.fillRect(this.x, this.y, this.width, this.height);
-    }
+////////// PART OF PLAYER [SPIDER MAN] //////////
+let init_player_w = 75;
+let init_player_h = 75;
+let init_player_x = 50;
+let init_player_y = canvas.height-init_player_h;
+function player(x, y, width, height) {
+    context.fillStyle = 'green';
+    context.fillRect(x, y, width, height)
 }
 
-const enemy = new Enemy(300);
-enemy.Draw();
+function jump() {
+    let dist_jump = 80;
+    player(init_player_x, init_player_y-dist_jump, init_player_w, init_player_h);
+}
 
+function slide() {
+    let dist_slide = init_player_h / 2
+    player(init_player_x, init_player_y+dist_slide, init_player_w, dist_slide);
+}
+
+function running(event) {
+    event = event || window.event;
+    if (event.keyCode == '39') { // button RIGHT
+        player(init_player_x, init_player_y, init_player_w, init_player_h);
+    } else if (event.keyCode == '38') { // button UP
+        jump();
+    } else if (event.keyCode == '40') { // button DOWN
+        slide();
+    }
+}
+document.onkeydown = running;
